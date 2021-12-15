772f297728bbd635219122e66cc8959a83b45524
Rexer-Glitch <gumboraymacantony@gmail.com>
2021-12-16 01:30:18 +0300

    😄another experimental commit --> webpack configs

--- Calculator/webpack.config.js
+++ Calculator/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "production",
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public/js"),
     filename: "main.bundle.js",
   },
   module: {
